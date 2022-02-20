7bdd4f022b7e4d1bcc3290dc48eba938ad46b1fe
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2022-02-20 19:35:45 +0100

    Added bentoml model saving logic

--- metaflow_pipeline.py
+++ metaflow_pipeline.py
@@ -1,15 +1,23 @@
 from metaflow import FlowSpec, step
-
 from train import train, test
 
+# from bento_deployment import FashionMNISTClassifier
+import bentoml
 
-class ExperimentalPyTorchFlow(FlowSpec):
+
+class FashionMNISTFlow(FlowSpec):
+    def __init__(self, use_cli=True):
+        super().__init__(use_cli)
+        self.epochs = None
+        self.model = None
+        self.test_result = None
 
     @step
     def start(self):
         self.epochs = 1
         self.next(self.model_train)
 
+    # @resources(memory=8196, cpu=8)
     @step
     def model_train(self):
         self.model = train(num_epochs=self.epochs)
@@ -17,14 +25,22 @@ class ExperimentalPyTorchFlow(FlowSpec):
 
     @step
     def model_test(self):
-        self.outputs = test(self.model)
+        self.test_result = test(self.model)
+        self.next(self.bentoml_deployment)
+
+    @step
+    def bentoml_deployment(self):
+        model_name = "fashion_mnist_classifier"
+        tag = bentoml.pytorch.save(
+            name=model_name, model=self.model, metadata=self.test_result
+        )
+        print(tag)
         self.next(self.end)
 
     @step
     def end(self):
-        print(self.outputs)
+        print(self.test_result)
 
 
-if __name__ == '__main__':
-    obj = ExperimentalPyTorchFlow()
-
+if __name__ == "__main__":
+    obj = FashionMNISTFlow()
