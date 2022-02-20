e31bc85ac98004620ba6bce448f775f840bdf205
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2022-02-20 19:35:28 +0100

    Code lint

--- model.py
+++ model.py
@@ -3,7 +3,6 @@ import torch.nn.functional as F
 
 
 class FashionMNISTConvnet(nn.Module):
-
     def __init__(self):
         super(FashionMNISTConvnet, self).__init__()
 
@@ -11,14 +10,14 @@ class FashionMNISTConvnet(nn.Module):
             nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1),
             nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
+            nn.MaxPool2d(kernel_size=2, stride=2),
         )
 
         self.convlayer2 = nn.Sequential(
             nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3),
             nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.MaxPool2d(2)
+            nn.MaxPool2d(2),
         )
 
         self.fc1 = nn.Linear(in_features=64 * 6 * 6, out_features=600)
